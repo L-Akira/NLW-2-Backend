c733eb63e6cf0621dc47df5f26d366204800ac9d
Leonardo Akira <leonardoakira08@gmail.com>
2020-08-08 12:50:50 -0300

    first commit of backend

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
+
+
+
+const routes = express.Router();
+const classesController = new ClassesController;
+const connectionController = new ConnectionController;
+
+
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
+
+routes.get('/connections', connectionController.index);
+routes.post('/connections', connectionController.create);
+
+export default routes;
